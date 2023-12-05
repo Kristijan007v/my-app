61cc43b1dfe2783efbcc9efe98320e2569e71e69
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2023-12-05 01:57:10 +0100

    feat: Push test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            'https://foto-zeljko-dev.b-cdn.net',
-        ],
+        domains: ['localhost', 'foto-zeljko-dev.b-cdn.net'],
     },
 }
 
